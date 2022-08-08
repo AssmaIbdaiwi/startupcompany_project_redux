64b4955ce281732ac9d0761f2a1397afbd65688b
ola-kanakri <ola.kanakri12@gmail.com>
2022-08-08 07:07:07 +0300

    ola

--- front_end/src/components/Profile.js
+++ front_end/src/components/Profile.js
@@ -268,7 +268,7 @@ const Profile = () => {
               // styles={{ backgroundImage:`url(${data.image})` }}
               >
                 <img
-                  src="https://pfpmaker.com/_nuxt/img/profile-3-1.3e702c5.png"
+                  src="https://www.pngall.com/wp-content/uploads/5/Profile-Female.png"
                   style={{ width: "75%", height: "75%" }}
                   alt="image"
                 />
